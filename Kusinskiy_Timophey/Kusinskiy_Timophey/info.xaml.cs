2252d8ffbddf58f343da2074b44e770178e81f77
Qdime^ <70803605+Tsimafei-Qdime@users.noreply.github.com>
2025-03-17 13:21:00 +0300

    Final

---
+++ Kusinskiy_Timophey/Kusinskiy_Timophey/info.xaml.cs
@@ -0,0 +1,50 @@
+﻿using CoreLibarary.Models;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace Kusinskiy_Timophey
+{
+    /// <summary>
+    /// Логика взаимодействия для info.xaml
+    /// </summary>
+    public partial class info : Window
+    {
+        private Dish dish;
+        public info(Dish list)
+        {
+            InitializeComponent();
+            dish = list;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var stream = new MemoryStream(dish.image);
+                var bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = stream;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.EndInit();
+
+                imageOfProduct.Source = bitmapimage;
+
+                txtName.Text = dish.ProductName;
+                txtDescription.Text = dish.Description;
+                txtCost.Text = Convert.ToString(dish.Cost) + " Руб";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Can't view information: '{ex.Message}'", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+    }
+}
